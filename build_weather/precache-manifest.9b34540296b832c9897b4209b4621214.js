f1c8aabfe72d878be19096a67201a2cf0b3ab66a
yawagyu <yawagyu@gmail.com>
2020-06-22 13:31:11 +0200

    added-build

---
+++ build_weather/precache-manifest.9b34540296b832c9897b4209b4621214.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e240399f71b70066e21a5e62a581e9b",
+    "url": "/build_weather/index.html"
+  },
+  {
+    "revision": "4948db339870bc1213b6",
+    "url": "/build_weather/static/css/main.fe733b48.chunk.css"
+  },
+  {
+    "revision": "688a66c604faa362653f",
+    "url": "/build_weather/static/js/2.26e3d674.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/build_weather/static/js/2.26e3d674.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4948db339870bc1213b6",
+    "url": "/build_weather/static/js/main.b142a0ad.chunk.js"
+  },
+  {
+    "revision": "1d2679585a8ea56217a4",
+    "url": "/build_weather/static/js/runtime-main.95e3c613.js"
+  },
+  {
+    "revision": "6908a233c4d934fd7799de3f3b05b96d",
+    "url": "/build_weather/static/media/clear.6908a233.png"
+  },
+  {
+    "revision": "02a7aee1b6e489ce098068cbe3c71643",
+    "url": "/build_weather/static/media/clouds.02a7aee1.png"
+  },
+  {
+    "revision": "7e6304ce0de9952d28da6147e1fc01b5",
+    "url": "/build_weather/static/media/rain.7e6304ce.png"
+  },
+  {
+    "revision": "efd969c4d232fc162392d7180ae91e10",
+    "url": "/build_weather/static/media/snow.efd969c4.png"
+  }
+]);
